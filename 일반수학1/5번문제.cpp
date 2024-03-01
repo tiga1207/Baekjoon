b6afcb3f274ceeeb89b6f43c2ddacb6d447190f3
tiga1207 <tiga1207@naver.com>
2024-03-02 00:05:39 +0900

    일반수학 4번까지

---
+++ 일반수학1/5번문제.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    //1 2 9 22 41 66
+    //차이: 1 7 13 19 25
+    //차이에 대한 규칙 6씩 늘어남
+    
+    //1 3 11 25 45
+    //차이: 2 8 14 20
+
+    return 0;
+}
